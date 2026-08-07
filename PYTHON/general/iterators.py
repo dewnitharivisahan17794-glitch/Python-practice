8592992b8da348ef5e3cbe219cabc98c0066d217
Dewnitha Rivisahan <dewnitharivisahan17794@gmail.com>
2026-08-07 12:45:59 +0530

    iteration

--- PYTHON/general/iterators.py
+++ PYTHON/general/iterators.py
@@ -18,6 +18,33 @@ while True:
     except StopIteration:
         break              
 
-
  #both methord are correct , but easies and fastest way is last one.
 
+
+#make new itaration
+
+class myit:
+    def __init__(self):
+        self.x=2
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        val=self.x
+        self.x+=2
+        return val
+
+myObj=myit()
+
+itr=iter(myObj)
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
