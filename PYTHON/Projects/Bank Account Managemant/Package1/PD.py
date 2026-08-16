548b272c66e20381676d1600efb873a07ce1b6e1
Dewnitha Rivisahan <dewnitharivisahan17794@gmail.com>
2026-08-16 14:11:08 +0530

    2026/8/16

--- PYTHON/Projects/Bank Account Managemant/Package1/PD.py
+++ PYTHON/Projects/Bank Account Managemant/Package1/PD.py
@@ -1,5 +1,6 @@
 
 import json
+import os
 
 
 class PD():
@@ -7,7 +8,9 @@ class PD():
         self._Account_Number = 11234566783
         self._Account_Holder_Name = "B C P Rathnayake"
         self._Serial_Num = "2700-56"
-        self.file_path = "Update.json"
+        package_dir = os.path.dirname(os.path.abspath(__file__))
+        main_dir = os.path.dirname(package_dir)
+        self.file_path = os.path.join(main_dir, "data_base", "Update.json")
         try:
             with open(self.file_path, "r") as file:
                 data = json.load(file)
